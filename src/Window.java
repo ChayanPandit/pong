31fb3df777d3d53fb9b1eb96a89a918fae4057c7
ChayanPandit <chayanpandit07@gmail.com>
2024-10-13 15:11:07 +0530

    chore: game loop and fps calculation

--- src/Window.java
+++ src/Window.java
@@ -1,7 +1,10 @@
 import javax.swing.JFrame;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
 
 public class Window extends JFrame implements Runnable {
+
+    Graphics2D g2;
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -9,11 +12,31 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g2 = (Graphics2D) this.getGraphics();
+    }
+
+    public void update(double dt) {
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+        System.out.println(dt + " seconds passed since last frame");
+        System.out.println(1/dt + " fps");
     }
 
     @Override
     public void run() {
+        double lastFrameTime = 0.0;
         while (true) {
+            double time = Time.getTime();
+            double deltatime = time - lastFrameTime;
+            lastFrameTime = time;
+
+            update(deltatime);
+
+            try {
+                Thread.sleep(30);
+            } catch (Exception e) {
+
+            }
         }
     }
 
